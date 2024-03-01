03aa7998be3f3249192ea65ccb1c4d700e410540
Peter Spolar <spolarpeter86@gmail.com>
2024-03-01 16:44:59 +0100

    added yt link

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -5,12 +5,13 @@ type Props = {};
 
 function Media({}: Props) {
   return (
-    <div className="mt-32 text-white flex flex-col md:items-center p-2 md:p-0">
+    <div className="mt-32 text-white flex flex-col md:items-center p-2 md:p-0 gap-2">
       <div className="aspect-video md:w-[853px]">
         <iframe
           className="h-full w-full"
-          src="https://www.youtube.com/embed/5JPghLhYohw"
-          title="Projekt Krank - Sorg"
+          src="https://youtu.be/kdPTc8gFr7Y
+          "
+          title="Projekt Krank - Alter Mann"
           width="100%"
         ></iframe>
       </div>
